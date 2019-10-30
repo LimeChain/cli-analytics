e8351cbc74eac05b0cfde46ad083405c0c2ace9b
Desimira <desimira@limechain.tech>
2019-10-30 16:20:41 +0200

    add docker-compose.yml file

--- database/models/cliCommandData.js
+++ database/models/cliCommandData.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const CliCommandDataSchema = new mongoose.Schema({
     type: {
         type: String,
-        unique: true,
         required: true,
         trim: true
     },
