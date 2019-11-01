e3da8bdf03b332591be1edd2f0b42558a5f2458b
Desimira <desimira@limechain.tech>
2019-11-01 14:48:59 +0200

    add custom header key

--- middlewares/identification.js
+++ middlewares/identification.js
@@ -1,6 +1,6 @@
 class Identification {
     static async validateApiID(req, res, next) {
-        if (!req.headers.id || (req.headers.id != process.env.API_ID)) {
+        if (!req.headers['X-Cli-Analytics-App-Id'] || (req.headers['X-Cli-Analytics-App-Id'] != process.env.API_ID)) {
             console.error('No API ID was passed as header for identification or ID not valid.');
             res.status(403).send('Unauthorized');
         }
