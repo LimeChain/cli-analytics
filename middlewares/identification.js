13149f5b041cc52f0579d456db41cfee608626c0
Desimira <desimira@limechain.tech>
2019-11-01 15:25:08 +0200

    header key with lower case letters

--- middlewares/identification.js
+++ middlewares/identification.js
@@ -1,6 +1,6 @@
 class Identification {
     static async validateApiID(req, res, next) {
-        if (!req.headers['X-Cli-Analytics-App-Id'] || (req.headers['X-Cli-Analytics-App-Id'] != process.env.API_ID)) {
+        if (!req.headers['x-cli-analytics-app-id'] || (req.headers['x-cli-analytics-app-id'] != process.env.API_ID)) {
             console.error('No API ID was passed as header for identification or ID not valid.');
             res.status(403).send('Unauthorized');
         }
